a1b6e911724fcdc18c3f9fa8c26d8ce959ae28a1
Ivandro Evora <ivandroe@caixa.cv>
2026-01-20 14:23:38 -0100

    metadados crédito, motivos de encaminhamento, fincc e carta proposta, remoção de pdf-viewer, atualização dos pacotes, configuração inicial do vite e melhorias

--- src/redux/slices/suporte-cliente.js
+++ src/redux/slices/suporte-cliente.js
@@ -212,7 +212,7 @@ export function updateInSuporte(item, body, params) {
         (item === 'toggle-prompts' && `/api/v1/mail-scan-presets/toggle-active/${params?.id}`) ||
         // ticket
         (item === 'assign' && `/api/v1/tickets/assign/${params?.id}/${params?.value?.id}`) ||
-        (item === 'core-validation' && `/api/v1/customers/validate-core-bank/${params?.id}`) ||
+        // (item === 'core-validation' && `/api/v1/customers/validate-core-bank/${params?.id}`) ||
         (item === 'change-department' && `/api/v1/tickets/change-department/${params?.id}/${params?.value?.id}`) ||
         (item === 'change-status' &&
           `/api/v1/tickets/change-status/${params?.id}/${params?.value?.id}?resolved=${!!params?.resolved}`) ||
